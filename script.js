ef06f9302fcd3faafec8fa0d0f80d8f141a7a9fe
as7ronau7 <scott@scott-nguyen.com>
2024-07-31 20:40:43 -0500

    Restructured code to be cleaner

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
 const grid = document.querySelector('.grid');
-
 const btn = document.getElementById('btn');
+let gridSize = 16;
 
+function defaultGrid(size) {
+  // create grid of divs within container
+  for (let i = 1; i <= size; i++) {
+    for (let j = 1; j <= size; j++) {
+      // create pixel and assign class name
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      grid.appendChild(pixel)
+      pixel.style.width += `calc(960px / ${size})`;
+      pixel.style.height += `calc(960px / ${size})`;
+    }
+  }
+  // calls the colorGrid() to change pixel color
+  colorGrid();
+}
 
-// create grid of divs within container
-for (let i = 1; i <= 20; i++) {
-  for (let j = 1; j <= 20; j++) {
-    // create pixel and assign class name
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    grid.appendChild(pixel)
+function resizeGrid(size = 16) {
+  // if grid has child elements then remove them
+  if (grid.hasChildNodes()) {
+    grid.innerHTML = '';
+  }
+
+  // create grid of divs within container
+  for (let i = 1; i <= size; i++) {
+    for (let j = 1; j <= size; j++) {
+      // create pixel and assign class name
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      grid.appendChild(pixel)
+      pixel.style.width += `calc(960px / ${size})`;
+      pixel.style.height += `calc(960px / ${size})`;
+    }
+  }
+  // calls the colorGrid() to change pixel color
+  colorGrid();
+
+}
+
+function colorGrid() {
+  // Array.from turns a HTMLCollection into an array that you can then loop through
+  const tiles = Array.from(document.querySelectorAll('.pixel'));
+  for (const tile of tiles) {
+    tile.addEventListener('mouseover', () => {
+      tile.style.backgroundColor += "black";
+    });
   }
 }
 
-const tiles = Array.from(document.querySelectorAll('.pixel'));
-
-for (const tile of tiles) {
-  tile.addEventListener('mouseover', e => {
-    tile.style.backgroundColor = "black";
-  });
-}
+// Resize Grid Button Event Listener
+btn.addEventListener('click', () => {
+  const ask = Number.parseInt(prompt("Enter the grid size you would like: "))
+  ask > 100 || ask < 0 ? alert('Please enter a number between 0 and 100') : ask;
+  resizeGrid(ask);
+})
+// sets the default grid on the screen
+defaultGrid(gridSize);
